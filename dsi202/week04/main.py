77c7c5f760191f33dcbe3006749b0984942d29c5
Wasit Limprasert <wasit7@gmail.com>
2019-09-05 15:12:56 +0700

    log user datetime visiting

--- dsi202/week04/main.py
+++ dsi202/week04/main.py
@@ -1,15 +1,26 @@
 from aiohttp import web
+import sqlite3
+from datetime import datetime
+
+def usercounter(name):
+    conn = sqlite3.connect('usercounter.db')
+    c = conn.cursor()
+
+    username=name
+    now=datetime.now()
+    time=now.isoformat()
+
+    c.execute("INSERT INTO usercounter VALUES ('%s','%s')"%(time,username))
+    conn.commit()
+    conn.close()
 
 async def handle(request):
     name = request.match_info.get('name', "Anonymous")
     text = "Hello, " + name
+    usercounter(name)
     return web.Response(text=text)
 
-async def counter(request):
-    return web.Response(text="count"+"5")
-
 app = web.Application()
-app.add_routes([web.get('/', handle),
-                web.get('/counter/', counter)])
+app.router.add_get('/counter/{name}', handle)
 
-web.run_app(app,port=8081)
+web.run_app(app,port=8082)
