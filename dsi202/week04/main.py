7f8244d9ced08108f784a438b57e42e8102de839
Wasit Limprasert <wasit7@gmail.com>
2019-09-05 16:04:30 +0700

    add list view

--- dsi202/week04/main.py
+++ dsi202/week04/main.py
@@ -2,6 +2,17 @@ from aiohttp import web
 import sqlite3
 from datetime import datetime
 
+html="""
+<!DOCTYPE html>
+<html>
+<body>
+<h1>My First Heading</h1>
+<p>Hello %s</p>
+%s
+</body>
+</html>
+"""
+
 def usercounter(name):
     conn = sqlite3.connect('usercounter.db')
     c = conn.cursor()
@@ -17,8 +28,20 @@ def usercounter(name):
 async def handle(request):
     name = request.match_info.get('name', "Anonymous")
     text = "Hello, " + name
+    table = """
+    <table>
+        <tr>
+            <th>Datetime</th>
+            <th>Name</th>
+        </tr>
+        <tr>
+            <td>2019-9-5</td>
+            <td>Maria Anders</td>
+        </tr>
+    </table>    
+    """
     usercounter(name)
-    return web.Response(text=text)
+    return web.Response(text=html%(name,table),content_type='text/html')
 
 app = web.Application()
 app.router.add_get('/counter/{name}', handle)
