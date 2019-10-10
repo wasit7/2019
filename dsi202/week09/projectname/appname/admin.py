16ab31746650d678136e4461011fa5536af2d667
Wasit Limprasert <wasit7@gmail.com>
2019-10-10 16:14:48 +0700

    add list_field... to admin.py

--- dsi202/week09/projectname/appname/admin.py
+++ dsi202/week09/projectname/appname/admin.py
@@ -3,4 +3,7 @@ from .models import Customer, Car, Rent
 
 class CarAdmin(admin.ModelAdmin):
     fields = ('brand', 'model', 'price')
+    list_display = ('id','brand', 'model', 'price')
+    list_filter = ('model',)
+    list_editable = ('brand', 'model', 'price')
 admin.site.register(Car, CarAdmin)
